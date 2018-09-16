91aefbf3e7e21368e404c6267a6427c1d0291e13
Greg Chamberlain <gregchamberlain94@gmail.com>
2018-09-16 01:14:21 -0400

    Moved rollup config into config/rollup/

--- config/rollup/rollup.config.js
+++ config/rollup/rollup.config.js
@@ -1,5 +1,5 @@
 import commonjs from 'rollup-plugin-commonjs';
-import pkg from './package.json';
+import pkg from '../../package.json';
 import resolve from 'rollup-plugin-node-resolve';
 import sourcemaps from 'rollup-plugin-sourcemaps';
 
